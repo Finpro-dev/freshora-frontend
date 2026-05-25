66022634fc4563fe46db0e1a82d86900ac1b3d54
Novpa <aa.novparadiputra@binus.ac.id>
2026-05-25 13:01:05 +0800

    ref/cleaning up junk codes

--- app/payment/_components/PayButton.tsx
+++ app/payment/_components/PayButton.tsx
@@ -4,15 +4,11 @@ import { useEffect } from "react";
 
 function PayButton() {
   useEffect(() => {
-    // You can also change below url value to any script url you wish to load,
-    // for example this is snap.js for Sandbox Env (Note: remove `.sandbox` from url if you want to use production version)
     const midtransScriptUrl = "https://app.sandbox.midtrans.com/snap/snap.js";
 
     const scriptTag = document.createElement("script");
     scriptTag.src = midtransScriptUrl;
 
-    // Optional: set script attribute, for example snap.js have data-client-key attribute
-    // (change the value according to your client-key)
     const myMidtransClientKey = process.env.NEXT_PUBLIC_MIDTRANS_CLIENT_KEY!;
     scriptTag.setAttribute("data-client-key", myMidtransClientKey);
 
