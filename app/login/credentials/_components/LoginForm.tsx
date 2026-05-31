2f7aab8ad7e0caafd73eada46d8b2fcb644c7f67
Novpa <aa.novparadiputra@binus.ac.id>
2026-05-31 19:56:28 +0800

    fix: correct import path for login schema in LoginForm component

--- app/login/credentials/_components/LoginForm.tsx
+++ app/login/credentials/_components/LoginForm.tsx
@@ -4,11 +4,7 @@ import { loginUser } from "@/actions/login-user";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
-<<<<<<< Updated upstream
-import { LoginInput, loginSchema } from "../../_schema/loginSchema";
-=======
 import { LoginInput, loginSchema } from "../../_schema/login-schema";
->>>>>>> Stashed changes
 import AuthNavigation from "./AuthNavigation";
 import { redirect } from "next/navigation";
 
