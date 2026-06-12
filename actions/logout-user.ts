a8e116b86c3a28c347347ad92b1076f78b7c50b2
Novpa <aa.novparadiputra@binus.ac.id>
2026-06-12 12:44:06 +0800

    fix: issue during logout

--- actions/logout-user.ts
+++ actions/logout-user.ts
@@ -2,14 +2,21 @@
 
 import { forwardExpressCookie } from "@/shared/utils/cookie-forwarder-util";
 import axios from "axios";
+import { cookies } from "next/headers";
 
 export const logoutUser = async () => {
+  const cookieStore = await cookies();
+  const accessToken = cookieStore.get("accessToken")?.value;
+  const refreshToken = cookieStore.get("refreshToken")?.value;
   let res;
   try {
     res = await axios.post(
       `${process.env.NEXT_PUBLIC_API_URL}/auth/logout`,
       {},
       {
+        headers: {
+          Cookie: `accessToken=${accessToken}; refreshToken=${refreshToken};`,
+        },
         withCredentials: true,
       },
     );
