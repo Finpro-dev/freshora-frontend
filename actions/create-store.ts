9810395753a48709f5b16e3704429bbda55b7c28
Novpa <aa.novparadiputra@binus.ac.id>
2026-06-22 09:52:45 +0800

    wip: create new store

---
+++ actions/create-store.ts
@@ -0,0 +1,35 @@
+"use server";
+
+import { CORS_CREDENTIALS } from "@/shared/config/dotenv-config";
+import { forwardExpressCookie } from "@/shared/utils/cookie-forwarder-util";
+import axios from "axios";
+import { revalidatePath } from "next/cache";
+import { cookies } from "next/headers";
+
+export const createNewStore = async (formData: FormData) => {
+  const allCookie = await cookies();
+  const accessToken = allCookie.get("accessToken")?.value;
+  const refreshToken = allCookie.get("refreshToken")?.value;
+  try {
+    const res = await axios.post(
+      `${CORS_CREDENTIALS.API_BASE_URL}/addresses/`,
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          Cookie: `accessToken=${accessToken}; refreshToken=${refreshToken};`,
+        },
+      },
+    );
+
+    await forwardExpressCookie(res.headers["set-cookie"]);
+    revalidatePath("/dashboard/store");
+    return { data: res.data.data, success: true };
+  } catch (error: any) {
+    const errorMessage =
+      error.response?.data?.message ||
+      "There's something wrong with the server!";
+
+    return { success: false, error: errorMessage };
+  }
+};
---
+++ app/(dashboard)/dashboard/store/new/_schemas/create-store-schema.ts
@@ -0,0 +1,55 @@
+import { z } from "zod";
+
+const phoneRegex = /^(?:\+62|62|08)[2-9]\d{7,11}$/;
+
+export const createStoreSchema = z.object({
+  name: z
+    .string()
+    .min(3, "Store name must be at least 3 characters long")
+    .max(30, "Store name cannot exceed 30 characters")
+    .optional(),
+
+  address: z
+    .string()
+    .min(5, "Address must be at least 5 characters long")
+    .max(100, "Address cannot exceed 100 characters")
+    .optional(),
+
+  phone: z
+    .string()
+    .min(1, { message: "Phone number is required" })
+    .min(9, { message: "Phone number is too short" })
+    .max(15, { message: "Phone number cannot exceed 15 characters" })
+    .regex(phoneRegex, {
+      message: "Invalid phone number format. Use 08... or +62...",
+    })
+    .optional(),
+
+  district: z
+    .string()
+    .min(2, "District name is too short")
+    .max(50, "District cannot exceed 50 characters")
+    .optional(),
+
+  city: z
+    .string()
+    .min(2, "City name is too short")
+    .max(50, "City cannot exceed 50 characters")
+    .optional(),
+
+  province: z
+    .string()
+    .min(2, "Province name is too short")
+    .max(50, "Province cannot exceed 50 characters")
+    .optional(),
+
+  postalCode: z
+    .string()
+    .length(5, "Postal code must be exactly 5 digits")
+    .regex(/^\d+$/, "Postal code must contain numbers only")
+    .optional(),
+
+  userId: z.uuid("Invalid User Id"),
+});
+
+export type CreateStoreInput = z.infer<typeof createStoreSchema>;
